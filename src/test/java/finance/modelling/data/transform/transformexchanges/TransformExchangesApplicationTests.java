62d435110dad0d6e1e02dd387bb743b5dce3762f
keirbailey <keirinvestment@gmail.com>
2020-09-21 20:24:01 +0100

    Initial commit w/ fully parameterised app.yml file w/ relevant integration config

---
+++ src/test/java/finance/modelling/data/transform/transformexchanges/TransformExchangesApplicationTests.java
@@ -0,0 +1,13 @@
+package finance.modelling.data.transform.transformexchanges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransformExchangesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
