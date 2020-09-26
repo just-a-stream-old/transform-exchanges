63fbdf77e7c90357df496905d97ee410bd8625b4
keirbailey <keirinvestment@gmail.com>
2020-09-26 14:51:36 +0100

    Added improved db write logging for success and fail w/ dbUri

--- src/main/java/finance/modelling/data/transform/transformexchanges/service/ExchangeDataModelServiceImpl.java
+++ src/main/java/finance/modelling/data/transform/transformexchanges/service/ExchangeDataModelServiceImpl.java
@@ -1,11 +1,11 @@
 package finance.modelling.data.transform.transformexchanges.service;
 
 import finance.modelling.data.transform.transformexchanges.repository.ExchangeRepository;
+import finance.modelling.data.transform.transformexchanges.repository.config.MongoDbConfig;
 import finance.modelling.data.transform.transformexchanges.repository.mapper.ExchangeMapper;
 import finance.modelling.data.transform.transformexchanges.service.config.TopicConfig;
 import finance.modelling.fmcommons.data.logging.LogDb;
 import finance.modelling.fmcommons.data.logging.kstream.LogMessageConsumed;
-import finance.modelling.fmcommons.data.logging.kstream.LogMessageSent;
 import finance.modelling.fmcommons.data.schema.eod.dto.EodExchangeDTO;
 import finance.modelling.fmcommons.data.schema.model.Exchange;
 import lombok.extern.slf4j.Slf4j;
@@ -23,10 +23,12 @@ public class ExchangeDataModelServiceImpl implements ExchangeDataModelService {
 
     private final TopicConfig topics;
     private final ExchangeRepository exchangeRepository;
+    private final MongoDbConfig dbConfig;
 
-    public ExchangeDataModelServiceImpl(TopicConfig topics, ExchangeRepository exchangeRepository) {
+    public ExchangeDataModelServiceImpl(TopicConfig topics, ExchangeRepository exchangeRepository, MongoDbConfig dbConfig) {
         this.topics = topics;
         this.exchangeRepository = exchangeRepository;
+        this.dbConfig = dbConfig;
     }
 
     @Bean
@@ -44,11 +46,10 @@ public class ExchangeDataModelServiceImpl implements ExchangeDataModelService {
                 .flatMap(exchangeRepository::save)
                 .subscribe(
                         exchangeMono -> LogDb.logInfoDataItemSaved(
-                                Exchange.class, exchangeMono.getExchangeCode(), "pathToDb"),
+                                Exchange.class, exchangeMono.getExchangeCode(), dbConfig.getDbUri()),
                         error -> LogDb.logErrorFailedDataItemSave(
-                                Exchange.class, error, "pathToDb", List.of("Log and fail"))
+                                Exchange.class, error, dbConfig.getDbUri(), List.of("Log and fail"))
                 );
 
     }
-
 }
