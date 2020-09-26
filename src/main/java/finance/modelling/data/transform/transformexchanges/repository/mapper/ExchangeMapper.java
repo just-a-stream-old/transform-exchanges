9d1037aa37a38d5469a453df3e25c024417f0aa8
keirbailey <keirinvestment@gmail.com>
2020-09-26 14:41:59 +0100

    Added resository mono integration w/ logging

--- src/main/java/finance/modelling/data/transform/transformexchanges/repository/mapper/ExchangeMapper.java
+++ src/main/java/finance/modelling/data/transform/transformexchanges/repository/mapper/ExchangeMapper.java
@@ -2,10 +2,11 @@ package finance.modelling.data.transform.transformexchanges.repository.mapper;
 
 import finance.modelling.fmcommons.data.schema.eod.dto.EodExchangeDTO;
 import finance.modelling.fmcommons.data.schema.model.Exchange;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
+import finance.modelling.fmcommons.data.schema.model.FinanceApi;
+import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ExchangeMapper {
@@ -16,4 +17,9 @@ public interface ExchangeMapper {
         @Mapping(source = "operatingMICs", target = "marketIdCode")
     })
     Exchange exchangeEodDTOToExchange(EodExchangeDTO dto);
+
+    @AfterMapping
+    default void determineFinanceApisField(EodExchangeDTO dto, @MappingTarget Exchange exchange) {
+        exchange.setFinanceApis(List.of(FinanceApi.EOD_HISTORICAL_DATA));
+    }
 }
--- src/main/java/finance/modelling/data/transform/transformexchanges/service/ExchangeDataModelServiceImpl.java
+++ src/main/java/finance/modelling/data/transform/transformexchanges/service/ExchangeDataModelServiceImpl.java
@@ -1,25 +1,31 @@
 package finance.modelling.data.transform.transformexchanges.service;
 
 import finance.modelling.data.transform.transformexchanges.repository.ExchangeRepository;
+import finance.modelling.data.transform.transformexchanges.repository.mapper.ExchangeMapper;
+import finance.modelling.data.transform.transformexchanges.service.config.TopicConfig;
+import finance.modelling.fmcommons.data.logging.LogDb;
 import finance.modelling.fmcommons.data.logging.kstream.LogMessageConsumed;
+import finance.modelling.fmcommons.data.logging.kstream.LogMessageSent;
 import finance.modelling.fmcommons.data.schema.eod.dto.EodExchangeDTO;
+import finance.modelling.fmcommons.data.schema.model.Exchange;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.streams.kstream.KStream;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 @Service
+@Slf4j
 public class ExchangeDataModelServiceImpl implements ExchangeDataModelService {
 
-    private final String inputEodExchangesTopic;
+    private final TopicConfig topics;
     private final ExchangeRepository exchangeRepository;
 
-    public ExchangeDataModelServiceImpl(
-            @Value("${spring.cloud.stream.bindings.generateExchangeDataModel-in-0.destination}") String inputEodExchangesTopic,
-            ExchangeRepository exchangeRepository) {
-        this.inputEodExchangesTopic = inputEodExchangesTopic;
+    public ExchangeDataModelServiceImpl(TopicConfig topics, ExchangeRepository exchangeRepository) {
+        this.topics = topics;
         this.exchangeRepository = exchangeRepository;
     }
 
@@ -27,8 +33,22 @@ public class ExchangeDataModelServiceImpl implements ExchangeDataModelService {
     public Consumer<KStream<String, EodExchangeDTO>> generateExchangeDataModel() {
 
         return eodExchanges -> eodExchanges
-                .transformValues(() -> new LogMessageConsumed<>("x-trace-id"))
-                .peek((key, value) -> System.out.println(value.toString()));
+                .transformValues(() -> new LogMessageConsumed<>(topics.getTraceIdHeaderName()))
+                .mapValues(ExchangeMapper.INSTANCE::exchangeEodDTOToExchange)
+                .foreach(this::saveToExchangeRepository);
+    }
+
+    protected void saveToExchangeRepository(String _key, Exchange exchange) {
+        Mono
+                .just(exchange)
+                .flatMap(exchangeRepository::save)
+                .subscribe(
+                        exchangeMono -> LogDb.logInfoDataItemSaved(
+                                Exchange.class, exchangeMono.getExchangeCode(), "pathToDb"),
+                        error -> LogDb.logErrorFailedDataItemSave(
+                                Exchange.class, error, "pathToDb", List.of("Log and fail"))
+                );
+
     }
 
 }
